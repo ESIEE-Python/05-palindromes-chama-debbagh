c1c06bf1021e317aa7060cee43f0a7c16998c01a
chama-debbagh <chama.debbagh@edu.esiee.fr>
2024-09-30 12:31:32 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
+""" Programme qui verifie si une chaine de caractere est un palindrome """
 #### Fonction secondaire
 
-
 def ispalindrome(p):
-
-    # votre code ici
-    
-    return False
-
+    """ FONCTION SECONDAIRE QUI DETERMINE SI UNE CHAINE
+     EST UN PALINDROME OU PAS TOUT EN ELIMINANT LES ACCENTS ET LES ESPACES """
+    table=str.maketrans('àâäéèêëîïôöùûüÿç' , 'aaaeeeeiioouuuyc' , " ")
+    p=p.lower().translate(table)
+    #suprime les caractere non alphabetique
+    caractere=''
+    for i in p:
+        if ('a' <= i <= 'z') or ('0' <= i <= '9') :
+            caractere += i
+    #verifie si c'est un palindrome
+    return caractere==caractere[::-1]
 #### Fonction principale
-
 
 def main():
 
     # vos appels à la fonction secondaire ici
+    """ fait les teste de la fonction secondaire et verifie si les exemple sont des palindrome"""
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
@@ -20,3 +26,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+#resultat si une chaine de caractere est un palindrome
